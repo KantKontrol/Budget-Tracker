9696e39d564df71c869d61ccc526f8fe7e9553b6
KantKontrol <nick.derissio.tech@gmail.com>
2020-04-28 17:02:21 -0400

    fixed path to stylesheet in service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,43 @@
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    "/style.css",
+    "/styles.css",
     "/index.js",
     "/manifest.webmanifest",
     "/icons/icon-192x192.png",
-    "/icons/icon-512x512"
+    "/icons/icon-512x512.png"
   ];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
+
+
+//cache files
+self.addEventListener("install", function(evt) {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  
+    self.skipWaiting();
+});
+
+//removes all old cache data
+self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
